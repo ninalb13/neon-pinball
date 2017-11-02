13d42d3e8e443d070d6362f123a76e13f131e1a1
Nina López Bobet <ninalb@TTC.CAT>
2017-11-02 19:51:24 +0100

    Updated Background

--- neon-pinball/Globals.h
+++ neon-pinball/Globals.h
@@ -21,7 +21,7 @@ enum update_status
 };
 
 // Configuration -----------
-#define SCREEN_WIDTH 500
+#define SCREEN_WIDTH 560
 #define SCREEN_HEIGHT 923
 #define SCREEN_SIZE 1
 #define WIN_FULLSCREEN false
